e8f10d6ae5bbd4f00f051b504969885cc103a871
Sheepearrrrrrrrrr <mrkacsantusz@gmail.com>
2025-03-20 17:53:32 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
+/* ============= PROGRAM 1 START ============= */
+function lightemup1() {
+    let index = 0
+    for (let i of list1) {
+        for (let j = 4; j >= i; j--) {
+            led.plot(index, j)
+        }
+        index++;
+    }
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    let toSend = ""
+    lightemup1()
+    for (let i of list1) {
+        toSend += i;
+    }
+    radio.sendString(toSend)
+})
+
+let list1: number[] = [randint(0, 4), randint(0, 4), randint(0, 4), randint(0, 4), randint(0, 4)]
+radio.setGroup(147)
+/* ============= PROGRAM 1 END ============= */
 /* ============= PROGRAM 2 START ============= */
 function lightemup2() {
     let index = 0
